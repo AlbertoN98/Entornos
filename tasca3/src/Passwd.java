426a427eacf564a4a682d952d71bc7453b9f6b43
Kikoromero97 <kikoromero1997@gmail.com>
2023-05-08 18:32:14 +0200

    Clase Passwd creada Update 4.

--- tasca3/src/Passwd.java
+++ tasca3/src/Passwd.java
@@ -6,32 +6,47 @@ public class Passwd {
         int numDigit = 0;
         Map<Boolean, String> errorPass = new HashMap<>();
         boolean tieneMayus = false, tieneEspecial = false, tieneOcho = false;
+        boolean errorOcho = false, errorDigitos = false, errorMayus = false, errorEspecial = false;
         for (int i = 0; i < contrasenya.length(); i++) {
             caracter = contrasenya.charAt(i);
             if (contrasenya.length() >= 8) {
                 tieneOcho = true;
             } else {
-                errorPass.put(false, "La contrasenya ha de tenir almenys 8 caràcters\n");
+                errorOcho = true;
             }
             if (Character.isDigit(caracter)) {
                 numDigit++;
             } else {
-                errorPass.put(false, "La contasenya ha de tenir almenys 2 números\n");
+                errorDigitos = true;
             }
             if (Character.isUpperCase(caracter)) {
                 tieneMayus = true;
             } else {
-                errorPass.put(false, "La contrasenya ha de contenir almenys una lletra majúscula\n");
+                errorMayus = true;
             }
             if (contrasenya.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?].*")) {
                 tieneEspecial = true;
             } else {
-                errorPass.put(false, "La contrasenya ha de contenir almenys un caràcter especial");
+                errorEspecial = true;
             }
         }
         if (numDigit >= 2 && tieneMayus && tieneEspecial && tieneOcho) {
             errorPass.put(true, "");
+        } else {
+            if (errorOcho) {
+                errorPass.put(false, "La contraseña debe tener al menos 8 caracteres");
+            }
+            if (errorDigitos) {
+                errorPass.put(false, "La contraseña debe tener al menos 2 dígitos");
+            }
+            if (errorMayus) {
+                errorPass.put(false, "La contraseña debe tener al menos una letra mayúscula");
+            }
+            if (errorEspecial) {
+                errorPass.put(false, "La contraseña debe tener al menos un carácter especial");
+            }
         }
         return errorPass;
     }
+
 }
