881e015a062ff079cd31f823152f9933b14096ac
Kikoromero97 <kikoromero1997@gmail.com>
2023-05-08 18:13:09 +0200

    Clase Passwd creada Update 2.

--- tasca3/src/Passwd.java
+++ tasca3/src/Passwd.java
@@ -5,20 +5,31 @@ public class Passwd {
         char caracter;
         int numDigit = 0;
         Map<Boolean, String> errorPass = new HashMap<>();
-        boolean tieneMayus = false, tieneEspecial = false;
+        boolean tieneMayus = false, tieneEspecial = false, tieneOcho = false;
         for (int i = 0; i < contrasenya.length(); i++) {
             caracter = contrasenya.charAt(i);
+            if (contrasenya.length() >= 8) {
+                tieneOcho = true;
+            } else {
+                errorPass.put(false, "La contrasenya ha de tenir almenys 8 caràcters");
+            }
             if (Character.isDigit(caracter)) {
                 numDigit++;
+            } else {
+                errorPass.put(false, "La contasenya ha de tenir almenys 2 números");
             }
             if (Character.isUpperCase(caracter)) {
                 tieneMayus = true;
+            } else {
+                errorPass.put(false, "La contrasenya ha de contenir almenys una lletra majúscula");
             }
-            if (contrasenya.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?].*")) {
+            if (contrasenya.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?].*")) {
                 tieneEspecial = true;
+            } else {
+                errorPass.put(false, "La contrasenya ha de contenir almenys un caràcter especial");
             }
         }
-        if (numDigit >= 2 && tieneMayus && tieneEspecial && contrasenya.length() >= 8) {
+        if (numDigit >= 2 && tieneMayus && tieneEspecial && tieneOcho) {
             errorPass.put(true, "Contraseña válida.");
         } else {
             errorPass.put(false, "La contraseña es inválida");
