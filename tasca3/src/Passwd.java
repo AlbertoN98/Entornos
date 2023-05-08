24903cea896b54d421dbe95b961fbc65bdf391be
Kikoromero97 <kikoromero1997@gmail.com>
2023-05-08 17:57:27 +0200

    Clase Passwd creada.

--- tasca3/src/Passwd.java
+++ tasca3/src/Passwd.java
@@ -5,44 +5,24 @@ public class Passwd {
         char caracter;
         int numDigit = 0;
         Map<Boolean, String> errorPass = new HashMap<>();
-        boolean tieneNumeros = false, tieneMayus = false, tieneEspecial = false,
-                tieneOcho = false;
+        boolean tieneMayus = false, tieneEspecial = false;
         for (int i = 0; i < contrasenya.length(); i++) {
             caracter = contrasenya.charAt(i);
-            if (contrasenya.length() == 8) {
-                tieneOcho = true;
-            } else {
-                errorPass.put(false, "Debe de ser mayor de 8 carácteres.");
-                return errorPass;
-            }
             if (Character.isDigit(caracter)) {
                 numDigit++;
-                if (numDigit == 2) {
-                    tieneNumeros = true;
-                }
-            } else {
-                errorPass.put(false, "Debe de contener 2 números como minimo.");
-                return errorPass;
             }
             if (Character.isUpperCase(caracter)) {
                 tieneMayus = true;
-            } else {
-                errorPass.put(false, "Debe de contener una mayúscula como minimo.");
-                return errorPass;
             }
             if (contrasenya.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?].*")) {
                 tieneEspecial = true;
-            } else {
-                errorPass.put(false, "Debe de contener algún caracter especial.");
-                return errorPass;
             }
         }
-        if (tieneNumeros == true && tieneMayus == true && tieneOcho == true && tieneEspecial == true) {
+        if (numDigit >= 2 && tieneMayus && tieneEspecial && contrasenya.length() >= 8) {
             errorPass.put(true, "Contraseña válida.");
-            return errorPass;
         } else {
-            errorPass.put(false, "La contraseña es inválida.");
-            return errorPass;
+            errorPass.put(false, "La contraseña es inválida");
         }
+        return errorPass;
     }
 }
